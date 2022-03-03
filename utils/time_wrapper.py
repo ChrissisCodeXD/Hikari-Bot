ae206c7d5271c37c281b2d2d91eb448b589d406c
Chrissi <schwarzlichtampel@gmail.com>
2022-03-03 14:29:37 +0100

    util time_wrapper: a wrapper to change the timestamp of a class

---
+++ utils/time_wrapper.py
@@ -0,0 +1,11 @@
+from functools import wraps
+import pytz
+import datetime
+
+def time_wrapper(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        self.pytz = pytz.timezone('Europe/Berlin')
+        self.time = datetime.datetime.now(tz=self.pytz)
+        return func(self, *args, **kwargs)
+    return wrapper
