4864059a444fe34224e3296adaa3fab6fe1e5072
Chrissi <schwarzlichtampel@gmail.com>
2022-03-03 14:24:03 +0100

    clear: added clear command

---
+++ Bot/extensions/moderation/clear.py
@@ -0,0 +1,30 @@
+import hikari
+import lightbulb.checks
+
+from imports import *
+
+clear_plugin = lightbulb.Plugin("clear")
+clear_plugin.add_checks(
+    lightbulb.checks.guild_only,
+    lightbulb.checks.bot_has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
+    lightbulb.checks.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
+)
+
+
+@clear_plugin.command()
+@lightbulb.option("messages", "Messages to delete", int, required=True)
+@lightbulb.command("clear", "Clears the Current Channel")
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def clear(ctx: lightbulb.Context) -> None:
+    channel: hikari.GuildTextChannel = ctx.get_channel()
+    await utils.purge(channel.id, ctx.options.messages, clear_plugin.bot)
+    await ctx.respond(f"Deleted {ctx.options.messages if ctx.options.messages <= 100 else 100} Messages!",
+                      delete_after=4)
+
+
+def load(bot):
+    bot.add_plugin(clear_plugin)
+
+
+def unload(bot):
+    bot.remove_plugin(clear_plugin)
