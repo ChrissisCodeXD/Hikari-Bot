fe17497e65f4f0269a8f62f70716625b89bf2396
Chrissi <schwarzlichtampel@gmail.com>
2022-03-05 04:42:37 +0100

    time_wrapper.py: cleanup

--- utils/time_wrapper.py
+++ utils/time_wrapper.py
@@ -2,10 +2,12 @@ from functools import wraps
 import pytz
 import datetime
 
-def time_wrapper(func):
+def class_time_wrapper(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         self.pytz = pytz.timezone('Europe/Berlin')
         self.time = datetime.datetime.now(tz=self.pytz)
         return func(self, *args, **kwargs)
     return wrapper
+
+
