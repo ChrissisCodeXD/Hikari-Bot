aa612daa140b61cc35e8d59216e9f47385a46620
Chrissi <schwarzlichtampel@gmail.com>
2022-03-05 04:46:20 +0100

    error_handler.py: Some Changes

--- Bot/extensions/events/error_handler.py
+++ Bot/extensions/events/error_handler.py
@@ -2,11 +2,10 @@ import discord
 from imports import *
 from Bot import __prefix__, Logger
 
-
 err_plugin = lightbulb.Plugin("error_plugin")
 
-
 Log = Logger()
+
 
 @err_plugin.listener(lightbulb.CommandErrorEvent)
 async def on_error(event):
@@ -18,13 +17,13 @@ async def on_error(event):
         await Log.send_error_log(event.exception, event.context.invoked_with)
         raise event.exception
 
-    exception = event.exception.__cause__ or event.exception
+    exception = event.exception
 
     if isinstance(event.exception, lightbulb.errors.NotEnoughArguments):
         if isinstance(event.context, lightbulb.context.prefix.PrefixContext):
             if event.context.invoked_with == "clear":
                 return await event.context.respond(
-                    f"Please specify the number of messages to clear `e.g. {__prefix__}clear 10`",
+                    f"Please specify the number of messages to clear `e.g. {event.app._prefix__get_class.get_prefix(event.context.guild_id)}clear 10`",
                     delete_after=3
                 )
 
@@ -38,26 +37,31 @@ async def on_error(event):
         return await event.context.respond("You are not the owner of this bot.", delete_after=3)
 
     if isinstance(exception, lightbulb.CommandIsOnCooldown):
-        return await event.context.respond(f"This command is on cooldown. Retry in `{exception.retry_after:.2f}` seconds.",
-                                    delete_after=3)
+        return await event.context.respond(
+            f"This command is on cooldown. Retry in `{exception.retry_after:.2f}` seconds.",
+            delete_after=3)
 
     if isinstance(exception, lightbulb.errors.CommandNotFound):
         temp = {}
         for i in event.app.prefix_commands:
-            temp[str(i)]= utils.similar(i,event.context.invoked_with)
+            temp[str(i)] = utils.similar(i, event.context.invoked_with)
         temp = sorted(temp.items(), key=lambda x: x[1], reverse=True)
-        return await event.context.respond(f"The Command {event.context.invoked_with} does not exist!\nDid you mean {__prefix__}{temp[0][0]} or {__prefix__}{temp[1][0]}?",delete_after=5)
+        return await event.context.respond(
+            f"The Command {event.context.invoked_with} does not exist!\nDid you mean {__prefix__}{temp[0][0]} or {__prefix__}{temp[1][0]}?",
+            delete_after=5)
 
     if isinstance(exception, lightbulb.NotOwner):
-        return await event.context.respond("You are not the owner of this bot.",delete_after=3)
+        return await event.context.respond("You are not the owner of this bot.", delete_after=3)
 
     if isinstance(exception, lightbulb.errors.BotMissingRequiredPermission):
-        return await event.context.respond("The bot is missing one or more permissions required in order to run this command", delete_after=3)
+        return await event.context.respond(
+            "The bot is missing one or more permissions required in order to run this command", delete_after=3)
 
     if isinstance(exception, lightbulb.errors.MissingRequiredPermission):
-        return await event.context.respond("You are missing one or more permissions required in order to run this command", delete_after=3)
+        return await event.context.respond(
+            "You are missing one or more permissions required in order to run this command", delete_after=3)
 
-    await Log.send_error_log(event.exception,event.context.invoked_with)
+    await Log.send_error_log(event.exception, event.context.invoked_with)
     raise (event.exception)
 
 
